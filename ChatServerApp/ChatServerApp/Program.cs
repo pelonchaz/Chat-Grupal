07733ec5a45c85179612bff9e98531375b04ae4c
pelonchaz <kami-sama_no_kuso@live.cl>
2021-04-08 11:33:54 -0400

    Agrega servidor base socket

---
+++ ChatServerApp/ChatServerApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using ChatServerApp.Comunicación;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Int32.Parse(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            { 
+                while (true)
+                {
+                    //Obtener clientes
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando Clientes...");
+                    if (servidor.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Conexion Establecida!");
+                        Console.WriteLine("S: Hola Mundo Cliente!");
+                        servidor.Escribir("Hola Mundo Cliente!");
+                        string mensaje = servidor.Leer();
+                        Console.WriteLine("C:{0}", mensaje);
+                        servidor.CerrarConexion();
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No es posible iniciar servidor");
+                Console.ReadKey();
+            }
+        }
+    }
+}
